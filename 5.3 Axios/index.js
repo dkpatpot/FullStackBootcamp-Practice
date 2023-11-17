cbf12936e1a22e00ec30aee7742ffbd004933d30
dkpatpot <thanaphat2142@gmail.com>
2023-11-17 15:11:08 +0700

    Update learning 11/17/2023

--- 5.3 Axios/index.js
+++ 5.3 Axios/index.js
@@ -15,7 +15,6 @@ app.get("/", async (req, res) => {
   try {
     const response = await axios.get("https://bored-api.appbrewery.com/random");
     const result = response.data;
-    console.log(result);
     res.render("index.ejs", { data: result });
   } catch (error) {
     console.error("Failed to make request:", error.message);
@@ -26,17 +25,19 @@ app.get("/", async (req, res) => {
 });
 
 app.post("/", async (req, res) => {
-  console.log(req.body);
   try {
-
-    const response = await axios.get("https://bored-api.appbrewery.com/filter?type=education");
+    var type = req.body.type;
+    var participants = req.body.participants;
+    const response = await axios.get(`https://bored-api.appbrewery.com/filter?type=${type}&participants=${participants}`);
     const result = response.data;
+    var random = Math.floor(Math.random() * result.length);
     console.log(result);
-    res.render("index.ejs", { data: result });
+    console.log(random);
+    res.render("index.ejs", { data: result[random] });
   } catch (error) {
     console.error("Failed to make request:", error.message);
     res.render("index.ejs", {
-      error: error.message,
+      error: "No activities that match your criteria.",
     });
   }
   // Step 2: Play around with the drop downs and see what gets logged.
